cf937df95decc283b2ba64dc3896bd12cc699f9d
hadi <hadibakhshi277@gmail.com>
2022-05-16 20:20:56 +0430

    styles got better

--- src/components/AddNewTodo.tsx
+++ src/components/AddNewTodo.tsx
@@ -24,43 +24,42 @@ const options: Options[] = [
   { value: "شرکت", text: "شرکت" },
   { value: "نوشیدن", text: "نوشیدن" },
   { value: "غذا خوردن", text: "غذا خوردن" },
+  { value: "سایر", text: "سایر" },
 ];
 
 const AddNewTodo: React.FC<AddTodoProps> = () => {
   // state
   const [inputValue, setInputValue] = useState<TodoTypeAndValue[]>([]);
-  const [valueSetter, setValueSetter] = useState('')
-  const [selectValueReset, setSelectValueReset] = useState('')
+  const [valueSetter, setValueSetter] = useState("");
+  const [selectValueReset, setSelectValueReset] = useState("");
   // action dispathcer
   const dispatch = useDispatch();
 
   //  Handlers
-
   // input change handler
   const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue([{ valueOfInput: e.target.value }]);
-    setValueSetter(e.target.value)
+    setValueSetter(e.target.value);
   };
   // select change handler
   const selectChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value;
-    setInputValue([...inputValue, { valueOfSelect: value }]);
-    setSelectValueReset(e.target.value)
+    setInputValue([...inputValue, { valueOfSelect: e.target.value }]);
+    setSelectValueReset(e.target.value);
   };
   //from submit handler
   const todoSubmitHandler = (e: FormEvent) => {
     e.preventDefault();
-    if(inputValue.length === 1) {
-      alert('لطفا نوع تسک را مشخص کنید')
+    if (inputValue.length === 1) {
+      alert("لطفا نوع تسک را مشخص کنید");
     }
     dispatch(addTodo(inputValue));
     setInputValue([]);
-    setValueSetter('')
-    setSelectValueReset('')
+    setValueSetter("");
+    setSelectValueReset("");
   };
 
   return (
-    <section className="flex flex-col justify-center items-center p-4 bg-violet-300 rounded-lg shadow-md">
+    <section className="fixed top-1 inset-x-0 mx-2 flex flex-col justify-center items-center p-4 bg-purple-300 rounded-lg shadow-md">
       <form className="flex flex-col" onSubmit={todoSubmitHandler}>
         <label htmlFor="todo-text" className="font-mono font-bold pb-2">
           تسک خود را وارد کنید
@@ -85,14 +84,14 @@ const AddNewTodo: React.FC<AddTodoProps> = () => {
         >
           <option
             className="rounded-md font-mono bg-gray-50 overflow-hidden"
-            defaultValue='else'
+            defaultValue="else"
             disabled
           >
             گزینه ها
           </option>
           {options.map((item, index) => (
             <option
-              className="rounded-md bg-gray-100 cursor-pointer"
+              className="rounded-md bg-purple-100 cursor-pointer"
               key={index}
               value={item.value}
             >
@@ -102,7 +101,7 @@ const AddNewTodo: React.FC<AddTodoProps> = () => {
         </select>
 
         <button
-          className="fixed inset-x-1/3 bottom-2  bg-violet-600 text-white p-4 mt-2 rounded-lg"
+          className="fixed inset-x-1/3 md:static bottom-1  bg-purple-600 text-white font-semibold p-4 md:p-2 mt-2 rounded-lg hover:ring hover:right-1 hover:ring-offset-1 hover:ring-purple-400"
           type="submit"
         >
           اضافه کردن
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -14,22 +14,36 @@ const TodoList: React.FC<TodoListProps> = () => {
   const dispatch = useDispatch();
 
   return (
-    <section className="flex flex-col rounded-md overflow-hidden w-full mt-5 mx-auto">
-      <div className=" grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <section className="mt-[190px] md:mt-[250px] flex flex-col rounded-md w-full mx-auto mb-14">
+      <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {items.map((todo) => (
-          <ul className="bg-blue-50 flex flex-col rounded-md" key={todo.id}>
+          <ul className=" bg-purple-100 flex flex-col rounded-md" key={todo.id}>
             <li className="flex flex-row py-5 justify-around items-center">
-              <p>{todo.text}</p>
-                <input
-                  type="checkbox"
-                  name="taskcheck"
-                 className="cursor-pointer"
-                  onClick={() => dispatch(toggleTodo(todo.id))}
-                />
+              <p
+                className={
+                  todo.isCompleted
+                    ? "text-purple-500"
+                    : "text-purple-900 font-semibold"
+                }
+              >
+                {todo.text}
+              </p>
+              <input
+                type="checkbox"
+                name="taskcheck"
+                className="cursor-pointer"
+                onClick={() => dispatch(toggleTodo(todo.id))}
+              />
             </li>
-              <button className="bg-violet-600 text-white text-sm rounded-md p-1 m-1" onClick={() => dispatch(removeTodo(todo.id))}>
-                پاک کردن
-              </button>
+            <span className="text-center text-violet-300 mb-2">
+              {todo.todoType}
+            </span>
+            <button
+              className=" bg-purple-600 text-white text-sm rounded-md p-1 m-1 hover:ring hover:right-1 hover:ring-offset-1 hover:ring-purple-300"
+              onClick={() => dispatch(removeTodo(todo.id))}
+            >
+              پاک کردن
+            </button>
           </ul>
         ))}
       </div>
